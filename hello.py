877cfc3e4f8b0881de7d8c8a3126da36857a27a3
Shadow16 <infante.nestor41@gmail.com>
2017-08-30 23:13:52 -0500

    setup script

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python
+# -*- encoding: UTF-8 -*-
 
-print('Hello world')
-edad = int(input('How old are you?'))
-print('You are young', edad)
+def main():
+    print('Hello world')
+    edad = int(input('How old are you? '))
+    print('You are young', edad)
 
-print('Anithing else')
-print('hey')
+    print('Anithing else')
+    print('hey')
+
+
+if __name__ == '__main__':
+    main()
